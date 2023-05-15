72ae2ac7b74f306f74713c927fafbb77239a495b
Eliax00789 <el-p@web.de>
2023-05-15 19:24:28 +0200

    Initial commit

---
+++ src/main/java/com/eliax008789/zombsreloaded/ZombsReloaded.java
@@ -0,0 +1,17 @@
+package com.eliax008789.zombsreloaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZombsReloaded extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
